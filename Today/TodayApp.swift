21e9bf86e128f8a64a89f18ca795e0d47a91174f
AW <veselovwork@gmail.com>
2022-11-23 01:52:33 +0300

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Andrey on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
